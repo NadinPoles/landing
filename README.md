e3909fc09e36f33a46a25647bc66186fb202e0b4
1verapoles <nadinpoles@yandex.by>
2023-01-18 21:08:51 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Landing page
-
-Landing page is live at [https://nadinpoles.github.io/landing/](https://nadinpoles.github.io/landing/).
--- src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react-scripts" />
-declare module "react-slick";
-declare module "@fancyapps/ui/dist/fancybox.esm.js";
-declare module "react-anchor-link-smooth-scroll";
-declare module "react-bootstrap/Modal";
